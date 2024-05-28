da91757c346bdb7cd9e2bcafcf2cd703f5b126c3
Raquel Reis <raquelreis233@gmail.com>
2024-05-28 06:03:39 -0300

    test

--- testes.py
+++ testes.py
@@ -1,15 +1,13 @@
-import pytest
-
 from fastapi.testclient import TestClient
 
 from main import app
 
 client = TestClient(app)
 
-def test_ola_mundo():
+def test_ola_mundo_status_code():
     response = client.get("/")
     assert response.status_code == 200
     
-def test_ola_mundo():
+def test_ola_mundo_conteudo():
     response = client.get("/")
-    assert response.json == {"Ola":"Mundo"}
+    assert response.json() == {"Ola":"Mundo"}
